4b6fca947053841b6f1f79feecfdc78b350b124f
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-04-18 13:57:39 +0700

    remove unsued import

--- src/test/java/kk/kertaskerja/urusanservice/UrusanServiceApplicationTests.java
+++ src/test/java/kk/kertaskerja/urusanservice/UrusanServiceApplicationTests.java
@@ -4,7 +4,6 @@ import kk.kertaskerja.urusanservice.domain.Urusan;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
